44291f11ca5647162859785d025f08a56b9bf04d
sherrymamta-tech <sherry.mamta@gmail.com>
2026-09-11 12:19:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,19 @@
 const CACHE_NAME = "cricket-hub-v1";
 
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./news.html",
-  "./players.html",
-  "./teams.html",
-  "./stories.html",
-  "./scenarios.html",
-  "./match.html",
-  "./achievements.html",
-  "./live.html",
-  "./manifest.json",
-  "./icon.svg"
-];
-
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
   );
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request);
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
